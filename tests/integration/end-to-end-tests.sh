9bae78923019e190e7e5eb23c4c3bcb60ce6995b
Carolyn Hicks <xpatiate@gmail.com>
2024-05-13 16:28:28 +1000

    fix shellcheck

--- tests/integration/end-to-end-tests.sh
+++ tests/integration/end-to-end-tests.sh
@@ -6,10 +6,11 @@ SERIAL=test-end-to-end
 testWritesOptimisationPlan() {
     planPath=/tmp/backup/plan.json
     setSeconds=$(date +%s)
-    docker exec mosquitto mosquitto_pub -h mosquitto -p 1883 -t cmd/local/standby/${SERIAL}/plan -m '{"site_id":"integration-test-site","optimisation_timestamp":{"seconds":'$setSeconds',"nanos":0},"optimisation_intervals":[],"setpoint_type":1}'
+    docker exec mosquitto mosquitto_pub -h mosquitto -p 1883 -t cmd/local/standby/${SERIAL}/plan -m '{"site_id":"integration-test-site","optimisation_timestamp":{"seconds":'"$setSeconds"',"nanos":0},"optimisation_intervals":[],"setpoint_type":1}'
     sleep 3
     planSeconds=$(cat $planPath | jq .optimisation_timestamp.seconds)
     assertEquals "didn't find expected data in written plan" "$planSeconds" "$setSeconds"
 }
 
+# shellcheck disable=SC1091
 . shunit2/shunit2

566a691799412fa2b9bfa418d6c437925b7a871c
Carolyn Hicks <xpatiate@gmail.com>
2024-05-13 16:51:37 +1000

    Add integration test (#7)
    * write optimisation plan to file when received
    
    * Make plan read/write methods; provide backup file path to handler rather than to runtime read method
    
    * simple integration test
    
    * add remaining integration test files
    
    * use local json file and dynamic seconds value in integration test
    
    * allow manual run of validation workflows
    
    * fix shellcheck
    
    * install gotestsum
    
    * use the other gotestsum
    
    * use the other other gotestsum
    
    * correct coverprofile arg
    
    * only include active commands in mosquitto.conf

---
+++ tests/integration/end-to-end-tests.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+# requires https://github.com/kward/shunit2/blob/master/doc/RELEASE_NOTES-2.1.8.md
+
+SERIAL=test-end-to-end
+
+testWritesOptimisationPlan() {
+    planPath=/tmp/backup/plan.json
+    setSeconds=$(date +%s)
+    docker exec mosquitto mosquitto_pub -h mosquitto -p 1883 -t cmd/local/standby/${SERIAL}/plan -m '{"site_id":"integration-test-site","optimisation_timestamp":{"seconds":'"$setSeconds"',"nanos":0},"optimisation_intervals":[],"setpoint_type":1}'
+    sleep 3
+    planSeconds=$(cat $planPath | jq .optimisation_timestamp.seconds)
+    assertEquals "didn't find expected data in written plan" "$planSeconds" "$setSeconds"
+}
+
+# shellcheck disable=SC1091
+. shunit2/shunit2

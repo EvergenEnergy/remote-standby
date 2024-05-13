fa42e542c73790c39a764d72cec608244d870c86
Carolyn Hicks <xpatiate@gmail.com>
2024-05-13 16:16:56 +1000

    use local json file and dynamic seconds value in integration test

--- tests/integration/end-to-end-tests.sh
+++ tests/integration/end-to-end-tests.sh
@@ -4,14 +4,12 @@
 SERIAL=test-end-to-end
 
 testWritesOptimisationPlan() {
-    planPath=/tmp/written-plan.json
-    docker exec command_standby_local rm $planPath
-    publishPlan=$(docker exec mosquitto mosquitto_pub -h mosquitto -p 1883 -t cmd/local/standby/${SERIAL}/plan -m '{"site_id":"integration-test-site","optimisation_timestamp":{"seconds":1715566795,"nanos":0},"optimisation_intervals":[],"setpoint_type":1}')
+    planPath=/tmp/backup/plan.json
+    setSeconds=$(date +%s)
+    docker exec mosquitto mosquitto_pub -h mosquitto -p 1883 -t cmd/local/standby/${SERIAL}/plan -m '{"site_id":"integration-test-site","optimisation_timestamp":{"seconds":'$setSeconds',"nanos":0},"optimisation_intervals":[],"setpoint_type":1}'
     sleep 3
-    writtenPlan=$(docker exec command_standby_local cat $planPath)
-    planSeconds=$(echo $writtenPlan | jq .optimisation_timestamp.seconds)
-    assertEquals "didn't find expected data in written plan" "$planSeconds" "1715566795"
-    docker exec command_standby_local rm $planPath
+    planSeconds=$(cat $planPath | jq .optimisation_timestamp.seconds)
+    assertEquals "didn't find expected data in written plan" "$planSeconds" "$setSeconds"
 }
 
 . shunit2/shunit2

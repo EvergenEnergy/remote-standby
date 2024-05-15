536d26d2004177b97418f40650231b480458246a
Carolyn Hicks <carolyn.hicks@evergen.energy>
2024-05-15 15:22:01 +1000

    Publish errors the same way remote-command-handler does; differentiate read and write command topics

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -8,27 +8,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-/*
-func getTestConfigFromEnv(t *testing.T) config.Config {
-	t.Setenv("MQTT_BROKER_URL", "tcp://localhost:1883")
-	t.Setenv("MQTT_STANDBY_TOPIC", "cmd/site/standby/serial/#")
-
-	cfg, err := config.FromEnv()
-	assert.NoError(t, err)
-	return cfg
-}
-*/
-
 func getTestConfig() config.Config {
 	return config.Config{
+		SiteName:     "test",
+		SerialNumber: "device",
 		MQTT: config.MQTTConfig{
 			BrokerURL:    "tcp://localhost:1833",
-			StandbyTopic: "cmd/site/standby/serial/#",
+			StandbyTopic: "cmd/${SITE_NAME}/standby/${SERIAL_NUMBER}/#",
 		},
 	}
 }
 
-func TestReadConfig(t *testing.T) {
+func TestInterpolateVars(t *testing.T) {
 	cfg := getTestConfig()
 	assert.NotEmpty(t, cfg)
+
+	assert.Contains(t, cfg.MQTT.StandbyTopic, "${SITE_NAME}")
+	assert.Contains(t, cfg.MQTT.StandbyTopic, "${SERIAL_NUMBER}")
+
+	cfg.InterpolateEnvVars()
+	assert.NotContains(t, cfg.MQTT.StandbyTopic, "${SITE_NAME}")
+	assert.NotContains(t, cfg.MQTT.StandbyTopic, "${SERIAL_NUMBER}")
+	assert.EqualValues(t, cfg.MQTT.StandbyTopic, "cmd/test/standby/device/#")
 }

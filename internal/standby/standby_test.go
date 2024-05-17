07c4c5a4dd6e5908086ff95121340daedac7a27a
Carolyn Hicks <xpatiate@gmail.com>
2024-05-17 14:05:47 +1000

    reformat outage test for clarity

--- internal/standby/standby_test.go
+++ internal/standby/standby_test.go
@@ -47,15 +47,30 @@ func TestRunsAClient_Integration(t *testing.T) {
 func TestDetectsOutage(t *testing.T) {
 	cfg := getTestConfig()
 	svc := NewService(testLogger, cfg)
-	currentTime := time.Now()
 
-	threeSecondsAgo := time.Duration(-3 * time.Second)
-	svc.latestCommandReceived = currentTime.Add(threeSecondsAgo)
+	// Start at current time, latest command received 1 second ago
+	interval := time.Duration(2 * time.Second)
+	currentTime := time.Now()
+	svc.latestCommandReceived = currentTime.Add(-1 * time.Second)
+
+	// First check after 1 interval, command is recent, remain in standby
+	currentTime = currentTime.Add(1 * interval)
 	svc.CheckForOutage(currentTime)
 	assert.EqualValues(t, svc.mode, StandbyMode)
 
-	fiveSecondsAgo := time.Duration(-5 * time.Second)
-	svc.latestCommandReceived = currentTime.Add(fiveSecondsAgo)
+	// After 2 intervals, command timestamp exceeds threshold, switch to command mode
+	currentTime = currentTime.Add(2 * interval)
 	svc.CheckForOutage(currentTime)
 	assert.EqualValues(t, svc.mode, CommandMode)
+
+	// After 3 intervals, command timestamp still exceeds threshold, remain in command mode
+	currentTime = currentTime.Add(3 * interval)
+	svc.CheckForOutage(currentTime)
+	assert.EqualValues(t, svc.mode, CommandMode)
+
+	// After 4 intervals, new command received, resume standby mode
+	currentTime = currentTime.Add(4 * interval)
+	svc.latestCommandReceived = currentTime.Add(-1 * time.Second)
+	svc.CheckForOutage(currentTime)
+	assert.EqualValues(t, svc.mode, StandbyMode)
 }
